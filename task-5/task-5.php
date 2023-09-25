ed92bc6b153ab49d27147bfd455f3b65f252905b
unknown <mobarokhossain448@gmail.com>
2023-09-25 23:56:20 +0600

    task 5 added

---
+++ task-5/task-5.php
@@ -0,0 +1,16 @@
+<?php 
+
+
+function generatePassword($length){
+    $str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+    
+    $password="";
+    for($i=0;$i<$length;$i++){
+        $password .= $str[rand(0, strlen($str)  -1)];
+
+    }
+    return $password;
+}
+
+echo generatePassword(12);
+
